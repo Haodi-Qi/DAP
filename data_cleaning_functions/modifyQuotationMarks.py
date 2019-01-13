624a503bac0967b2d5216eaedff67ec05a47209a
Qi Haodi <haodi.qi.2017@business.smu.edu.sg>
2019-01-13 15:35:48 +0800

    Update modifyQuotationMarks.py
    add modifications for fields: title, speaker_occupation and name

--- data_cleaning_functions/modifyQuotationMarks.py
+++ data_cleaning_functions/modifyQuotationMarks.py
@@ -3,7 +3,7 @@ import ast
 
 def modifyQuotationMarks(all_data):
     '''
-    this function modifies the format of description, related_talks and tags so that the values can be written
+    this function modifies the format of some fields so that the values can be written
     in the csv file in one single cell, by replacing all "" with ''
     '''
     for row in all_data:
@@ -17,3 +17,12 @@ def modifyQuotationMarks(all_data):
         tags = str(ast.literal_eval(
             row['tags'])).replace("\"", "\'")
         row['tags'] = "\"" + tags + "\""
+
+        row['name'] = "\"" + \
+            row['name'].replace("\"", "\'") + "\""
+
+        row['speaker_occupation'] = "\"" + \
+            row['speaker_occupation'].replace("\"", "\'") + "\""
+
+        row['title'] = "\"" + \
+            row['title'].replace("\"", "\'") + "\""
