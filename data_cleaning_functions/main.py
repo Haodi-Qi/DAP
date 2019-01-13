d61b25ff41fa22feaaa805299b81110b7a54f254
Qi Haodi <haodi.qi.2017@business.smu.edu.sg>
2019-01-13 15:27:21 +0800

    update modifyQuotationMarks.py and main.py
    update the doc string and code of modifyQuotationMarks.py
    
    update main.py to encode file writing with utf-8

--- data_cleaning_functions/main.py
+++ data_cleaning_functions/main.py
@@ -7,19 +7,19 @@ from splitRatings import splitRatings
 from modifyQuotationMarks import modifyQuotationMarks
 from appendTagNum import appendTagNum
 
-with open('../ted_main.csv', 'r', encoding='utf8') as csvfile:
+with open('../data_test.csv', 'r', encoding='utf-8',) as csvfile:
     lines = csv.DictReader(csvfile)
     all_data = list(lines)
 
+    modifyQuotationMarks(all_data)
     appendTalkID(all_data)
     appendTitleType(all_data)
     appendTagNum(all_data)
-    modifyQuotationMarks(all_data)
     modifyDates(all_data)
     modifyURL(all_data)
     splitRatings(all_data)
 
-with open('../processed_data.csv', 'w') as updatedFile:
+with open('../processed_data.csv', 'w', encoding='utf-8') as updatedFile:
     write_header = False
     for row in all_data:
         if not write_header:
--- data_cleaning_functions/modifyQuotationMarks.py
+++ data_cleaning_functions/modifyQuotationMarks.py
@@ -4,10 +4,11 @@ import ast
 def modifyQuotationMarks(all_data):
     '''
     this function modifies the format of description, related_talks and tags so that the values can be written
-    in the csv file in one single cell
+    in the csv file in one single cell, by replacing all "" with ''
     '''
     for row in all_data:
-        row['description'] = "\"" + row['description'] + "\""
+        row['description'] = "\"" + \
+            row['description'].replace("\"", "\'") + "\""
 
         related_talks = str(ast.literal_eval(
             row['related_talks'])).replace("\"", "\'")
