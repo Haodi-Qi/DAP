d7efc7afe39da6188c61ff38700564c9b9daf786
Qi Haodi <haodi.qi.2017@business.smu.edu.sg>
2019-01-17 14:02:44 +0800

    Add findTopNTags.py & process_tag.py
    - moved main.py from data_cleaning_function out
    - added findTopNTags.py and process_tags.py
    - added tag_file.csv, which is the output file of process_tags.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import csv
+import time
+import data_cleaning_functions as f
+from data_cleaning_functions.appendID import appendTalkID
+from data_cleaning_functions.appendTitleType import appendTitleType
+from data_cleaning_functions.modifyDates import modifyDates
+from data_cleaning_functions.modifyURL import modifyURL
+from data_cleaning_functions.splitRatings import splitRatings
+from data_cleaning_functions.modifyQuotationMarks import modifyQuotationMarks
+from data_cleaning_functions.appendTagNum import appendTagNum
+from data_cleaning_functions.appendRelatedTalksNum import appendRelatedTalksNum
+
+
+with open('data_test.csv', 'r', encoding='utf-8') as csvfile:
+    lines = csv.DictReader(csvfile)
+    all_data = list(lines)
+
+    appendRelatedTalksNum(all_data)
+    appendTagNum(all_data)
+    modifyQuotationMarks(all_data)
+    appendTalkID(all_data)
+    appendTitleType(all_data)
+    modifyDates(all_data)
+    modifyURL(all_data)
+    splitRatings(all_data)
+
+
+with open('processed_data.csv', 'w', encoding='utf-8') as updatedFile:
+    write_header = False
+    for row in all_data:
+        if not write_header:
+            header = ",".join(row.keys()) + "\n"
+            updatedFile.write(header)
+            write_header = True
+
+        data = ""
+        for value in row.values():
+            data += str(value) + ','
+        data = data[:-1] + "\n"
+        updatedFile.write(data)
